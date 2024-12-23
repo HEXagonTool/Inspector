f4fe674f2760b1ba9273d0e4ea6e25fe25f56609
HEXagon developer <h3xagon.tool@gmail.com>
2024-12-23 21:06:32 +0000

    путь к шаблонам можно указать при инициализации

--- src/Inspector.php
+++ src/Inspector.php
@@ -6,13 +6,17 @@ class InspectorConfig {
     public $status = 'inactive'; 
     public $templates_path = __DIR__ . '/templates';
     public $can_see = [];
+
+    public function __construct($options = []){
+        $this->templates_path = $options['templates_path'] ?? $this->templates_path;
+    }
 }
 
 class Inspector {
     public static $config;
 
-    public static function init() {
-        self::$config = new InspectorConfig();
+    public static function init($options = []) {
+        self::$config = new InspectorConfig($options);
     }
 
     public static function start() {
