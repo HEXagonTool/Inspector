6470c654f391d57c3c52624df28c09a8ab57f913
HEXagon developer <h3xagon.tool@gmail.com>
2024-12-23 21:20:44 +0000

    декомпозиция ядра

--- src/Inspector.php
+++ src/Inspector.php
@@ -2,16 +2,6 @@
 
 namespace HEXagonTool\Inspector;
 
-class InspectorConfig {
-    public $status = 'inactive'; 
-    public $templates_path = __DIR__ . '/templates';
-    public $can_see = [];
-
-    public function __construct($options = []){
-        $this->templates_path = $options['templates_path'] ?? $this->templates_path;
-    }
-}
-
 class Inspector {
     public static $config;
 
---
+++ src/InspectorConfig.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace HEXagonTool\Inspector;
+
+class InspectorConfig {
+    public $status = 'inactive'; 
+    public $templates_path = __DIR__ . '/templates';
+    public $can_see = [];
+
+    public function __construct($options = []){
+        $this->templates_path = $options['templates_path'] ?? $this->templates_path;
+    }
+}
